b80a9390d20f9ac4253179a8441ec2e454014803
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-04-27 20:41:55 -0600

    first commit

---
+++ src/main/java/mx/tec/lab/TestBdd1Application.java
@@ -0,0 +1,13 @@
+package mx.tec.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBdd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBdd1Application.class, args);
+	}
+
+}
